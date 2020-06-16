08328cdc7508c0e51fd570d7ca5dcc6f08ff393d
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2020-06-15 19:58:17 -0500

    flit build system

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='multislice',
-      description='Multislice propogation for xrays',
-      url='https://github.com/sajid-ali-nu/multislice/',
-      author='Sajid',
-      author_email='sajidsyed2021@u.northwestern.edu',
-      packages=['multislice'],
-      zip_safe=False)
